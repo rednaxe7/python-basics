6789ba08819cba61b3f094af1c5f106ab9c1d514
Alexander <rednaxe7@gmail.com>
2019-04-21 10:43:30 -0400

    basic examples python

---
+++ estructuras-repetitivas/ejercicio11.py
@@ -0,0 +1,20 @@
+# Escribe un programa que diga si un número introducido por teclado es o no primo. 
+# Un número primo es aquel que sólo es divisible entre él mismo y la unidad. 
+# Nota: Es suficiente probar hasta la raíz cuadrada del número para ver si es 
+# divisible por algún otro número.
+
+es_primo = True
+numero_es_primo = int(input("Introduce un número para comprobar si es primo:"))
+if numero_es_primo < 2:
+ 	es_primo = False
+else:
+	for num in range(2, numero_es_primo):
+		if numero_es_primo % num == 0:
+			es_primo = False
+
+if es_primo:
+	print("Es Primo")
+else:
+	print("No es Primo")
+
+
